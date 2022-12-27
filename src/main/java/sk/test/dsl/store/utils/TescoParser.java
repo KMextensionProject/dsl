9bbcc2b14531fb226beac83a450afd6e4b0332be
Martin Krajcovic <mkrajcovic@gratex.com>
2022-12-27 15:32:05 +0100

    added TescoParser and TescoURLMapper implementations

---
+++ src/main/java/sk/test/dsl/store/utils/TescoParser.java
@@ -0,0 +1,87 @@
+package sk.test.dsl.store.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import sk.test.dsl.core.Category;
+import sk.test.dsl.core.HTMLProductParser;
+import sk.test.dsl.core.Product;
+
+public class TescoParser implements HTMLProductParser {
+
+	@Override
+	public List<Product> parseHtmlProductsInfo(Document htmlPage, Category productsCategory) {
+		List<Product> products = new ArrayList<>(250);
+
+		Elements tiles = htmlPage
+			.select(".a-productListing__productsGrid__element")
+			.select(".product__info-wrapper");
+
+		for (Element tile : tiles) {
+			String productName = tile.selectFirst(".product__name").text();
+			Element pricePartParent = tile.selectFirst(".product__secondary-text");
+			Elements pricePartChilds = pricePartParent.select(".product__price-cc-text");
+
+			// this means that there is no actual discount
+			if (pricePartChilds.size() < 2) {
+				continue;
+			}
+
+			String currentPrice = pricePartParent.select(".product__price-cc-text").get(1).text(); // tu padne smotana
+			String quantityWithUnit = pricePartParent.select("div").get(1).text();
+			Element discountPart = tile.selectFirst(".product__discount-percentage");
+			String discountPercentage;
+
+			// if there is no percentage discount we need to extract the discount in euro cents
+			if (discountPart == null) {
+				discountPart = tile.selectFirst(".product__discount");
+				discountPercentage = discountPart.select(".saved_price__text").text();
+			} else {
+				discountPercentage = discountPart.selectFirst(".discount-percentage__text").text().replace(" ", ""); // toto sa meni
+			}
+
+			String previousPrice = discountPart.select(".product__discount-normal-price").text();
+
+			Product product = new Product.ProductBuilder()
+				.withName(productName)
+				.withCategory(productsCategory)
+				.withMeasurementUnit(quantityWithUnit)
+				.withPercentageDiscount(discountPercentage)
+				.withPreviousPrice(parsePrice(previousPrice))
+				.withCurrentPrice(parsePrice(currentPrice))
+				.withBondToClubCard(true) // almost everything.. check for different state on web
+				.createProduct();
+
+			products.add(product);
+		}
+
+		return products;
+	}
+
+	private double parsePrice(String textPrice) {
+		String price = textPrice.replace(',', '.');
+		int delimiterIndex;
+		if ((delimiterIndex = price.indexOf(' ')) >= 0) {
+			double price1 = Double.parseDouble(price.substring(0, delimiterIndex));
+			double price2 = Double.parseDouble(price.substring(delimiterIndex));
+			return Math.min(price1, price2);
+		}
+		return Double.parseDouble(price);
+	}
+
+	public int getNumberOfAvailablePages(Document htmlPage) {
+		Elements pagination = htmlPage
+				.select(".ddl_plp_pagination")
+				.select(".label");
+
+		if (pagination == null || pagination.isEmpty()) {
+			return 1;
+		}
+		String lastPage = pagination.get(pagination.size() - 1).text().trim();
+		return Integer.parseInt(lastPage);
+	}
+}
---
+++ src/main/java/sk/test/dsl/store/utils/TescoURLMapper.java
@@ -0,0 +1,47 @@
+package sk.test.dsl.store.utils;
+
+import static sk.test.dsl.core.Category.CERSTVE_VYROBKY;
+import static sk.test.dsl.core.Category.MASO_UDENINY;
+import static sk.test.dsl.core.Category.MRAZENE_VYROBKY;
+import static sk.test.dsl.core.Category.NAPOJE;
+import static sk.test.dsl.core.Category.OVOCIE_ZELENINA;
+import static sk.test.dsl.core.Category.TRVANLIVE_POTRAVINY;
+
+import java.util.EnumMap;
+
+import sk.test.dsl.core.Category;
+import sk.test.dsl.core.CategoryURLMapper;
+
+public class TescoURLMapper implements CategoryURLMapper {
+
+	private static final String BASE_URL = "https://tesco.sk/akciove-ponuky/akciove-produkty/";
+	private static final EnumMap<Category, String> CATEGORY_ENDPOINT_MAP;
+
+	// TODO: review mappings since categories intersect
+	static {
+		CATEGORY_ENDPOINT_MAP = new EnumMap<>(Category.class);
+		CATEGORY_ENDPOINT_MAP.put(MASO_UDENINY, appendToBaseUrl("maso-ryby-a-udeniny/"));
+		CATEGORY_ENDPOINT_MAP.put(NAPOJE, appendToBaseUrl("napoje/"));
+		CATEGORY_ENDPOINT_MAP.put(CERSTVE_VYROBKY, appendToBaseUrl("mliecne-vyrobky-vajcia-a-pecivo/"));
+		CATEGORY_ENDPOINT_MAP.put(OVOCIE_ZELENINA, appendToBaseUrl("ovocie-a-zelenina/"));
+		CATEGORY_ENDPOINT_MAP.put(TRVANLIVE_POTRAVINY, appendToBaseUrl("trvanlive-potraviny/"));
+		CATEGORY_ENDPOINT_MAP.put(MRAZENE_VYROBKY, appendToBaseUrl("mrazene-potraviny/"));
+	}
+
+	private static String appendToBaseUrl(String uri) {
+		return BASE_URL + uri;
+	}
+
+	@Override
+	public EnumMap<Category, String> getCategoryURLMap() {
+		return new EnumMap<>(CATEGORY_ENDPOINT_MAP);
+	}
+
+	public String getPagedURLByCategory(Category category, int page) {
+		String pageUrl = CATEGORY_ENDPOINT_MAP.get(category);
+		if (page > 1) {
+			return pageUrl + "?page=" + page;
+		}
+		return pageUrl;
+	}
+}
