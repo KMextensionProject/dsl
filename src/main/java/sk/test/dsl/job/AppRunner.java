cb0d476fdd34df5a40fbbbc75a0187cb2253b4ed
Martin Krajcovic <mkrajcovic@gratex.com>
2022-12-29 09:14:18 +0100

    AppRunner - added Lidl products parsing example

--- src/main/java/sk/test/dsl/job/AppRunner.java
+++ src/main/java/sk/test/dsl/job/AppRunner.java
@@ -3,15 +3,16 @@ package sk.test.dsl.job;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 import sk.test.dsl.product.Category;
 import sk.test.dsl.product.Product;
-import sk.test.dsl.product.parser.TescoParser;
-import sk.test.dsl.product.parser.TescoURLMapper;
+import sk.test.dsl.product.parser.LidlParser;
+import sk.test.dsl.product.parser.LidlURLMapper;
 
 public class AppRunner {
 
@@ -36,25 +37,39 @@ public class AppRunner {
 //			writer.append(System.lineSeparator());
 //			entry.getValue().forEach(AppRunner::println);
 //		}
+//
+//		// Tesco
+//		TescoURLMapper tescoUrlMapper = new TescoURLMapper();
+//		TescoParser tescoParser = new TescoParser();
+//
+//		// ziskam si zakladnu URL 1. stranky pre danu kategoriu
+//		String baseCategoryUrl = tescoUrlMapper.getCategoryURLMap().get(Category.TRVANLIVE_POTRAVINY);
+//
+//		// ziskam si z nej pocet stranok, na kolkatich sa rozpinaju produkty tejto kategorie
+//		int pages = tescoParser.getNumberOfAvailablePages(Jsoup.connect(baseCategoryUrl).get());
+//
+//		// ziskam si podla poslednej strany vsetky produkty (od 1. strany) ako html dokument
+//		Document htmlPage = Jsoup.connect(tescoUrlMapper.getPagedURLByCategory(Category.TRVANLIVE_POTRAVINY, pages)).get();
+//
+//		// extrahujem z html stranky vsetky data o relevantnych produktoch a spravim si z nich java Produkty v konkretnej kategorii
+//		List<Product> products = tescoParser.parseHtmlProductsInfo(htmlPage, Category.TRVANLIVE_POTRAVINY);
+//
+//		// zobrazim si cely zoznam produktov
+//		products.forEach(AppRunner::println);
+		
+		
+		// Lidl
+		LidlURLMapper mapper = new LidlURLMapper();
+		LidlParser parser = new LidlParser();
 
-		// Tesco
-		TescoURLMapper tescoUrlMapper = new TescoURLMapper();
-		TescoParser tescoParser = new TescoParser();
-
-		// ziskam si zakladnu URL 1. stranky pre danu kategoriu
-		String baseCategoryUrl = tescoUrlMapper.getCategoryURLMap().get(Category.TRVANLIVE_POTRAVINY);
-
-		// ziskam si z nej pocet stranok, na kolkatich sa rozpinaju produkty tejto kategorie
-		int pages = tescoParser.getNumberOfAvailablePages(Jsoup.connect(baseCategoryUrl).get());
-
-		// ziskam si podla poslednej strany vsetky produkty (od 1. strany) ako html dokument
-		Document htmlPage = Jsoup.connect(tescoUrlMapper.getPagedURLByCategory(Category.TRVANLIVE_POTRAVINY, pages)).get();
-
-		// extrahujem z html stranky vsetky data o relevantnych produktoch a spravim si z nich java Produkty v konkretnej kategorii
-		List<Product> products = tescoParser.parseHtmlProductsInfo(htmlPage, Category.TRVANLIVE_POTRAVINY);
-
-		// zobrazim si cely zoznam produktov
+		List<Product> products = new ArrayList<>(150);
+		for (Map.Entry<Category, String> entry : mapper.getCategoryURLMap().entrySet()) {
+			List<Product> categoryProducts = parser.parseHtmlProductsInfo(Jsoup.connect(entry.getValue()).get(), entry.getKey());
+			products.addAll(categoryProducts);
+		}
+		
 		products.forEach(AppRunner::println);
+		println(products.size());
 	}
 
 	private static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
