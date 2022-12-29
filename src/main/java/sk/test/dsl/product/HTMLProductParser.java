85dbd69c531d8d07f0f948436fdf3fc9695814f5
Martin Krajcovic <mkrajcovic@gratex.com>
2022-12-29 09:12:30 +0100

    HTMLProductParser - declared IOException on interface method

--- src/main/java/sk/test/dsl/product/HTMLProductParser.java
+++ src/main/java/sk/test/dsl/product/HTMLProductParser.java
@@ -1,11 +1,12 @@
 package sk.test.dsl.product;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.jsoup.nodes.Document;
 
 public interface HTMLProductParser {
 
-	public List<Product> parseHtmlProductsInfo(Document htmlPage, Category productsCategory);
+	public List<Product> parseHtmlProductsInfo(Document htmlPage, Category productsCategory) throws IOException;
 
 }
